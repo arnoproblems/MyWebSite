4abeece31e84f6a2761e718978d6a45cbf3acd1a
Arno Springer <54511008+arnoproblems@users.noreply.github.com>
2019-11-25 17:27:32 -0500

    Fixing Image component

--- src/components/menu.js
+++ src/components/menu.js
@@ -19,4 +19,4 @@ const Menu = () => (
         </div>
     )
 
- export default Menu
+export default Menu
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, graphql } from "gatsby";
+import {Link, graphql} from "gatsby";
 import Layout from '../components/layout';
 
 export default function Template({data}) {
@@ -15,7 +15,7 @@ export default function Template({data}) {
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
         </Layout>
-        
+
     )
 }
 
